8d5173bf283ece89ecb8a9450bdd2205901d8880
osoagacblg <akbashuseyin3883@gmail.com>
2026-05-19 21:19:03 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const agaclar = [
     {
-        ad: "Çam Ağacı (Pinus)",
+        ad: 'Çam Ağacı (<span class="bilimsel">Pinus</span>)',
         bilgi: "3-5 yaş aralığındadır.Dallar üzerinde demetler halinde bulunan, sert ve sivri uçlu yaprakları vardır. Bu yapraklar her mevsim yeşil kalır.Ağacın uç kısımlarında yukarı doğru uzanan açık renkli kısımlar, ağacın bu bahar döneminde verdiği yeni sürgünlerdir.Kuraklığa ve soğuğa karşı oldukça dayanıklı bir türdür, bu yüzden peyzaj çalışmalarında ve ağaçlandırma alanlarında sıkça tercih edilir. ",
         resim: "images/agac3.jpg"
     },
@@ -13,7 +13,7 @@ function getId() {
 
 function agaciGoster(id) {
     const agac = agaclar[id - 1];
-    document.getElementById("agacAdi").innerText = agac.ad;
+    document.getElementById("agacAdi").innerHTML = agac.ad;
     document.getElementById("agacBilgi").innerText = agac.bilgi;
     document.getElementById("agacResim").src = agac.resim;
 }
